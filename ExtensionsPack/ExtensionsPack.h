c0fa158c475191a6639a821194c2d75b6c9781ae
Thorax <thorax@me.com>
2018-02-27 19:57:29 +0300

    Initial Commit

---
+++ ExtensionsPack/ExtensionsPack.h
@@ -0,0 +1,19 @@
+//
+//  ExtensionsPack.h
+//  ExtensionsPack
+//
+//  Created by Yaroslav Pasternak on 27.02.2018.
+//  Copyright © 2018 Thorax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExtensionsPack.
+FOUNDATION_EXPORT double ExtensionsPackVersionNumber;
+
+//! Project version string for ExtensionsPack.
+FOUNDATION_EXPORT const unsigned char ExtensionsPackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExtensionsPack/PublicHeader.h>
+
+
